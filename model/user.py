2e195a615789c27f30e2870d956c6831af24a200
Augusto Roberto Tavares Júnior <eogutin@outlook.com>
2025-10-08 02:28:58 -0300

    feat(user): add email and phone variables

--- model/user.py
+++ model/user.py
@@ -1,37 +1,35 @@
 from abc import ABC, abstractmethod
 
 from exceptions.invalid_cpf import InvalidCPFError
+from exceptions.invalid_email import InvalidEmailError
+from exceptions.invalid_phone import InvalidPhoneError
 from exceptions.is_not_instance import IsNotInstanceError
 from utils.cpf_checker import is_valid_cpf
+from utils.email_checker import is_valid_email
+from utils.phone_checker import is_valid_phone
 
 
 class User(ABC):
     @abstractmethod
-    def __init__(self, id: int, name: str, cpf: str, email: str):
+    def __init__(self, name: str, cpf: str, email: str, phone: str):
         self._id = None
         self._name = None
         self._cpf = None
         self._email = None
+        self._phone = None
 
-        if isinstance(id, int):
-            self._id = id
         if isinstance(name, str):
             self._name = name
         if isinstance(cpf, str):
             self._cpf = cpf
         if isinstance(email, str):
             self._email = email
+        if isinstance(phone, str):
+            self._phone = phone
 
     @property
     def id(self) -> int:
         return self._id
-
-    @id.setter
-    def id(self, value: int):
-        if isinstance(value, int):
-            self._id = value
-        else:
-            raise IsNotInstanceError("ID must be an integer")
 
     @property
     def username(self) -> str:
@@ -65,6 +63,23 @@ class User(ABC):
     @email.setter
     def email(self, value: str):
         if isinstance(value, str):
-            self._email = value
+            if is_valid_email(value):
+                self._email = value
+            else:
+                raise InvalidEmailError("Invalid email address")
         else:
             raise IsNotInstanceError("Email must be a string")
+
+    @property
+    def phone(self) -> str:
+        return self._phone
+
+    @phone.setter
+    def phone(self, value: str):
+        if isinstance(value, str):
+            if is_valid_phone(value):
+                self._phone = value
+            else:
+                raise InvalidPhoneError("Invalid phone number")
+        else:
+            raise IsNotInstanceError("Phone must be a string")
